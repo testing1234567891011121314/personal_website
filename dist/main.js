b79b5193ae1bd2d13493ef610c30e92a5e305934
testing1234567891011121314 <90443963+testing1234567891011121314@users.noreply.github.com>
2025-03-22 00:35:12 +0800

    redo website

--- dist/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const button = document.getElementById("clickMe");
-button.addEventListener("click", () => {
-    alert("Hello, TypeScript!");
-});
--- src/main.ts
+++
@@ -1,5 +0,0 @@
-const button = document.getElementById("clickMe") as HTMLButtonElement;
-
-button.addEventListener("click", () => {
-  alert("Hello, TypeScript!");
-});
