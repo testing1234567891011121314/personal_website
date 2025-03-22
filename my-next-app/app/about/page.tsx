d8ff9cdb775f863fc5b09361509499ef7f07e4e3
testing1234567891011121314 <90443963+testing1234567891011121314@users.noreply.github.com>
2025-03-22 13:14:09 +0800

    Update website
    - add button css
    - add youtube link for tutorial

--- my-next-app/app/about/page.tsx
+++ my-next-app/app/about/page.tsx
@@ -6,6 +6,7 @@ const aboutPage = () => {
         <div>
             <h1>About Page</h1>
             <a href="/about.html">View Full HTML</a>
+            <a href="https://www.youtube.com/watch?v=ZVnjOPwW4ZA">Tutorial for next.js</a>
         </div>
 
     )
--- my-next-app/app/components/Button/Button.tsx
+++ my-next-app/app/components/Button/Button.tsx
@@ -1,9 +1,10 @@
 'use client';
 import React from 'react'
+import styles from './Button.module.css'
 
 const Button = () => {
     return (
-        <div>
+        <div className={styles.button}>
             <button onClick={() => console.log("Click")}> This is a Button </button>
         </div>
     )
